fe9347a28fb36b467ce939c796d168b6254b4428
Cassandra Zimmerman <cassandrazimmerman@Cassandras-MacBook-Pro.local>
2026-04-01 08:58:21 -0600

    fix path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // If you use a custom domain or a username.github.io repo, change this to '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/casszplaysmusic/',
+  base: '/',
 })
